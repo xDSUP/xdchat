07be9c06751bc0f1cee4b5532337388a6fabb5e0
Dgcim1 <gindra2001@gmail.com>
2023-02-24 13:15:34 +0300

    Add username for messages

--- server-xdchat/src/main/kotlin/xdsup/voicechat/config/ApplicationConfig.kt
+++ server-xdchat/src/main/kotlin/xdsup/voicechat/config/ApplicationConfig.kt
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.*
 
-@EnableWebMvc
 @Configuration
 class ApplicationConfig {
     @Bean
